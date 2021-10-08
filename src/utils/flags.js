02d2feb18fe9be430fec9cd93c963382bbb25e95
Welly Siauw AWS <67753754+wellsiau-aws@users.noreply.github.com>
2021-10-08 11:23:50 -0700

    insert configurationFetchedHandler

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -1,15 +1,23 @@
 import Rox from 'rox-browser'
 
 export const Flags = {
-  sidebar: new Rox.Flag(false),
-  title: new Rox.Flag(false)
+    sidebar: new Rox.Flag(false),
+    title: new Rox.Flag(false)
 }
 
+export const configurationFetchedHandler = fetcherResults => {
+    console.log('The configuration status is: ' + fetcherResults.fetcherStatus)
+    if (fetcherResults.hasChanges && fetcherResults.fetcherStatus === 'APPLIED_FROM_NETWORK') {
+          window.location.reload(false)
+    } else if (fetcherResults.fetcherStatus === 'ERROR_FETCH_FAILED') {
+          console.log('Error occured! Details are: ' + fetcherResults.errorDetails)
+    }
+}
 
 async function initCloudBees () {
   const options = {
-  }
-
+    configurationFetchedHandler: configurationFetchedHandler
+}
   Rox.register('default', Flags)
   await Rox.setup(process.env.VUE_APP_CLOUDBEES_KEY, options)
 }
