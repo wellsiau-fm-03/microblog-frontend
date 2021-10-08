827f662c8eb94fddedb19b10e100cef0415d2b82
Welly Siauw AWS <67753754+wellsiau-aws@users.noreply.github.com>
2021-10-08 11:45:38 -0700

    Added setCustomBooleanProperty

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -1,4 +1,6 @@
 import Rox from 'rox-browser'
+import store from '../store'
+import { betaAccess } from './users'
 
 export const Flags = {
     sidebar: new Rox.Flag(false),
@@ -17,7 +19,9 @@ export const configurationFetchedHandler = fetcherResults => {
 async function initCloudBees () {
   const options = {
     configurationFetchedHandler: configurationFetchedHandler
-}
+  }
+  Rox.setCustomBooleanProperty('isLoggedIn', store.getters.isLoggedIn)
+  Rox.setCustomBooleanProperty('hasBetaAccess', betaAccess())
   Rox.register('default', Flags)
   await Rox.setup(process.env.VUE_APP_CLOUDBEES_KEY, options)
 }
